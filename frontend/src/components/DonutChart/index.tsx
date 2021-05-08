560cfa806a2e84a03c6c0051f98c396db2b2da25
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-05-08 14:09:06 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,28 @@
 
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/request';
+
+type CharData = {
+    labels : string[];
+    series:number[];
+}
+
 function Donutchart() {
+
+    //forma errada
+    let chartData : CharData = {labels:[],series:[]};
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then(response =>{
+        const data = response.data as SaleSum[];
+        const mylabels = data.map(x=> x.sellerName);
+        const myseries = data.map(x=> x.sum);
+        chartData =  {labels:mylabels,series:myseries};
+        console.log(chartData)
+    });
+
 
 
     const options = {
@@ -9,17 +31,18 @@ function Donutchart() {
         }
     }
 
-    const mockData = {
+    /**const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
     
-    
+    **/
+
     return (
 
         <Chart
-            options={{ ...options,labels: mockData.labels}}
-            series={mockData.series}   
+            options={{ ...options,labels: chartData.labels}}
+            series={chartData.series}   
             type='donut'
             height="240"
         />
