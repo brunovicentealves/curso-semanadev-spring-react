2fa00bbad6e58c67f4f7a765758fa0f85bfc2109
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-05-09 09:09:17 -0300

    Barchart integration

--- frontend/src/components/Barchart/index.tsx
+++ frontend/src/components/Barchart/index.tsx
@@ -1,7 +1,34 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/request';
+
+type SeriesData = {
+    name:string;
+    data:number[];
+}
+
+type ChartData = {
+    labels:{
+        categories:string[];
+    };
+    series:SeriesData[];
+}
+
 function Barchart() {
 
+    const[charData,setChartData]=useState<ChartData>({
+        labels:{
+            categories:[]
+        },
+        series:[{
+            name:"",
+            data:[]
+        }]
+    });
 
     const options = {
         plotOptions: {
@@ -11,7 +38,34 @@ function Barchart() {
         },
     };
 
-    const mockData = {
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then(response =>{
+            const data = response.data as SaleSuccess[];
+            const mylabels = data.map(x=> x.sellerName);
+            const myseries = data.map(x=>round(100.0*x.deals/x.visited,1));
+            
+            setChartData ({
+                labels:{
+                    categories:mylabels
+                },
+                series:[
+                    {
+                        name:"% Success",
+                        data: myseries
+                    }
+                ]
+
+            }) ;
+            
+        });
+    },[]);
+
+
+
+
+   /**  const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
@@ -23,14 +77,14 @@ function Barchart() {
         ]
     };
 
-
+    **/
 
 
     return (
 
         <Chart
-            options={{ ...options,xaxis: mockData.labels}}
-            series={mockData.series}   
+            options={{ ...options,xaxis: charData.labels}}
+            series={charData.series}   
             type='bar'
             height="240"
         />
